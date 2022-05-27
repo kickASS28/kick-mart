fdd2d24a8a627c9b30a3e8a0c9448d8c51e31d4c
kickASS28 <72094447+kickASS28@users.noreply.github.com>
2022-05-27 16:15:37 +0530

    first commit

---
+++ src/pages/CartPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <div>
+      <p>Cart</p>
+    </div>
+  );
+};
+
+export default CartPage;
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchPage = () => {
+  return (
+    <div>
+      <p>Search</p>
+    </div>
+  );
+};
+
+export default SearchPage;
