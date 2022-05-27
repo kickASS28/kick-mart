bc60d55adc3eadbd6f07cc81c3aada5c09bc3c15
kickASS28 <72094447+kickASS28@users.noreply.github.com>
2022-05-27 16:52:47 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
-
+import { Route, Routes, Navigate } from "react-router-dom";
+import CartPage from "./pages/CartPage";
+import HomePage from "./pages/HomePage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import SearchPage from "./pages/SearchPage";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <main>
+        <Routes>
+          <Route path="/*" element={<Navigate to="/products" />} />
+          <Route path="/products" element={<HomePage />} />
+          <Route path="/products/:productId" element={<ProductDetailsPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
-
 export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,1 @@
+export const APIKey = "pk_test_4358403553baddb4e1124f897bfed32b97670cce7e8e1";
