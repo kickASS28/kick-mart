7a337728090a042447472c7e731601a2356f57a3
kickASS28 <72094447+kickASS28@users.noreply.github.com>
2022-05-29 12:25:57 +0530

    responsive nav & footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,22 +17,25 @@ const Navigation = () => {
     >
       <Container className={classes.container}>
         <Navbar.Brand>Kick-Mart</Navbar.Brand>
-        <Nav className="me-auto">
-          <LinkContainer to="/products">
-            <Nav.Link>Products</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/search">
-            <Nav.Link>Explore</Nav.Link>
-          </LinkContainer>
-        </Nav>
-        <Nav>
-          <LinkContainer to="/cart">
-            <Nav.Link>
-              <HiShoppingCart size={30} color="gold" />
-              Cart
-            </Nav.Link>
-          </LinkContainer>
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <LinkContainer to="/products">
+              <Nav.Link>Products</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/search">
+              <Nav.Link>Explore</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          <Nav>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <HiShoppingCart size={30} color="gold" />
+                Cart
+              </Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
