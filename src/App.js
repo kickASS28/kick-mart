37ae1dc29785a666ab0b7044b9e4b19fb2a6eca9
kickASS28 <72094447+kickASS28@users.noreply.github.com>
2022-05-29 19:08:35 +0530

    making it better

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import React, { Suspense } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
+import LoadingSpinner from "./components/LoadingSpinner";
 import CartPage from "./pages/CartPage";
 import HomePage from "./pages/HomePage";
-import ProductDetailsPage from "./pages/ProductDetailsPage";
-import SearchPage from "./pages/SearchPage";
-import Navigation from "./components/Navbar";
-import Footer from "./components/Footer";
+const ProductDetailsPage = React.lazy(() =>
+  import("./pages/ProductDetailsPage")
+);
+const SearchPage = React.lazy(() => import("./pages/SearchPage"));
+const Navigation = React.lazy(() => import("./components/Navbar"));
+const Footer = React.lazy(() => import("./components/Footer"));
 function App() {
   return (
     <>
-      <Navigation />
-      <main>
-        <Routes>
-          <Route path="/*" element={<Navigate to="/products" />} />
-          <Route path="/products" element={<HomePage />} />
-          <Route path="/products/:productId" element={<ProductDetailsPage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-      </main>
-      <Footer />
+      <Suspense fallback={<LoadingSpinner />}>
+        <Navigation />
+        <main>
+          <Routes>
+            <Route path="/*" element={<Navigate to="/products" />} />
+            <Route path="/products" element={<HomePage />} />
+            <Route
+              path="/products/:productId"
+              element={<ProductDetailsPage />}
+            />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Suspense>
     </>
   );
 }
