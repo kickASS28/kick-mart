acf914b9217e919d5ec1131e2168c68ff2dfeca8
kickASS28 <72094447+kickASS28@users.noreply.github.com>
2022-05-29 19:16:41 +0530

    lazy loading

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,23 +1,10 @@
 import React from "react";
 import ProductList from "../components/products/ProductList";
 import { useSelector } from "react-redux";
-import { APIKey } from "../constants/constants";
-import { useDispatch } from "react-redux";
-import { storeActions } from "../store/store";
-import Commerce from "@chec/commerce.js";
 import LoadingSpinner from "../components/LoadingSpinner";
 
 const HomePage = () => {
-  const dispatch = useDispatch();
   const isLoading = useSelector((state) => state.store.isLoading);
-  const commerce = new Commerce(APIKey);
-
-  commerce.products.list().then((product) => {
-    dispatch(storeActions.setLoading(true));
-    dispatch(storeActions.setProducts(product.data));
-    dispatch(storeActions.setLoading(false));
-  });
-
   const products = useSelector((state) => state.store.products);
 
   if (isLoading) {
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { useDispatch } from "react-redux";
 import Explore from "../components/explore/Explore";
-import Commerce from "@chec/commerce.js";
-import { APIKey } from "../constants/constants";
-import { storeActions } from "../store/store";
+
+
 
 const SearchPage = () => {
-  const dispatch = useDispatch();
-  const commerce = new Commerce(APIKey);
 
-  commerce.categories.list().then((category) => {
-    dispatch(storeActions.setLoading(true));
-    dispatch(storeActions.setCategories(category.data));
-    dispatch(storeActions.setLoading(false));
-  });
+
   return (
     <div>
       <Explore />
