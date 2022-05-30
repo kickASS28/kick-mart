c7d444dd9bf7827d5552e94ccb6a3c44775b835d
kickASS28 <72094447+kickASS28@users.noreply.github.com>
2022-05-30 10:03:54 +0530

    store actions bug1

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import LoadingSpinner from "../components/LoadingSpinner";
 import ProductDetails from "../components/products/ProductDetails";
 import { fetchProductsAndCats } from "../store/store";
 
@@ -15,6 +16,12 @@ const ProductDetailsPage = () => {
 
   if (!products) {
     dispatch(fetchProductsAndCats());
+  }
+
+  const isLoading = useSelector((state) => state.store.isLoading);
+
+  if (isLoading) {
+    return <LoadingSpinner />;
   }
 
   return <ProductDetails product={products} />;
